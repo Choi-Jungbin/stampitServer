5df09f429c1a234142cb3e7e0eddd7011cc1ec0d
JB <wjdqls6704@naver.com>
2024-06-21 22:21:54 +0900

    conf: 프로젝트 생성

---
+++ stampitServer/src/main/java/com/example/stampitserver/StampitServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stampitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampitServerApplication.class, args);
+	}
+
+}
---
+++ stampitServer/src/test/java/com/example/stampitserver/StampitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stampitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StampitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
