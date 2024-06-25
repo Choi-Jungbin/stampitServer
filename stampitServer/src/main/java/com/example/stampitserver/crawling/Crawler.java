1ec56bd93b4e3f08ee01e1f11fa9605253c3120d
JB <wjdqls6704@naver.com>
2024-06-26 00:07:29 +0900

    feat: 크롤링 메서드 기본 작성

--- stampitServer/src/main/java/com/example/stampitserver/crawling/Crawler.java
+++ stampitServer/src/main/java/com/example/stampitserver/crawling/Crawler.java
@@ -1,10 +1,15 @@
 package com.example.stampitserver.crawling;
 
+import com.example.stampitserver.contest.Contest;
+import com.example.stampitserver.contest.Field;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Crawler {
     public void crawling(String url){
@@ -16,6 +21,29 @@ public class Crawler {
             e.printStackTrace();
         }
 
-        Elements elements = doc.select("");
+        assert doc != null;
+        Elements links = doc.select("div.tit > a");
+
+        for(Element link : links){
+            contestCrawling("https://www.wevity.com/" + link.attr("href"));
+        }
+    }
+
+    private void contestCrawling(String url){
+        Document doc = null;
+
+        try {
+            doc = Jsoup.connect(url).get();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        assert doc != null;
+        Elements elements = doc.select("div.contest-detail");
+
+        String title = elements.select("h6.tit").text();
+
+        Elements infos = doc.select("ul.cd-info-list li");
+
     }
 }
