d0e3b2e3b1440abd05111e602a2168f035b897c1
JB <wjdqls6704@naver.com>
2024-06-26 00:01:58 +0900

    feat: ContestJPARepository 작성

---
+++ stampitServer/src/main/java/com/example/stampitserver/contest/ContestJPARepository.java
@@ -0,0 +1,6 @@
+package com.example.stampitserver.contest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContestJPARepository extends JpaRepository<Contest, Long> {
+}
