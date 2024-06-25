7fb0d0b0a0ee6756b7b7b62ac7dfece0d02e045a
JB <wjdqls6704@naver.com>
2024-06-26 00:00:39 +0900

    feat: applicant enum 작성

---
+++ stampitServer/src/main/java/com/example/stampitserver/contest/Applicant.java
@@ -0,0 +1,19 @@
+package com.example.stampitserver.contest;
+
+import lombok.Getter;
+
+@Getter
+public enum Applicant {
+    NO_RESTRICTION("제한없음"),
+    GENERAL("일반인"),
+    UNIVERSITY("대학생"),
+    TEENAGER("청소년"),
+    CHILD("어린이"),
+    ETC("기타");
+
+    private final String applicant;
+    
+    Applicant(String s){
+        this.applicant = s;
+    }
+}
