300eea2d61a5cb335f6e688c96f1be729ce1d5c9
JB <wjdqls6704@naver.com>
2024-06-28 23:15:10 +0900

    feat: 크롤링 페이지 파싱 및 contest 객체 생성

--- stampitServer/src/main/java/com/example/stampitserver/crawling/Crawler.java
+++ stampitServer/src/main/java/com/example/stampitserver/crawling/Crawler.java
@@ -1,13 +1,16 @@
 package com.example.stampitserver.crawling;
 
+import com.example.stampitserver.contest.Applicant;
 import com.example.stampitserver.contest.Contest;
 import com.example.stampitserver.contest.Field;
+import com.example.stampitserver.contest.Prize;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -66,18 +69,46 @@ public class Crawler {
             content.append(detail.text()).append("\n");
         }
 
+        String[] date = contestData.get("접수기간").split(" ~ ");
+
         Contest contest = Contest.builder()
-                .contestName()
-                .fields()
-                .applicant()
-                .host()
-                .sponsor()
-                .receptionStart()
-                .receptionEnd()
-                .prize()
-                .firstPrize()
-                .url()
-                .content()
+                .contestName(title)
+                .fields(parseFields(contestData.get("분야")))
+                .applicant(parseApplicants(contestData.get("응모대상")))
+                .host(contestData.get("주최/주관"))
+                .sponsor(contestData.get("후원/협찬"))
+                .receptionStart(parseDate(date[0]))
+                .receptionEnd(parseDate(date[1]))
+                .prize(Prize.fromString(contestData.get("총 상금")))
+                .firstPrize(contestData.get("1등 상금"))
+                .url(contestData.get("홈페이지"))
+                .content(content.toString())
                 .build();
+        System.out.println(contest);
+    }
+
+    private Set<Field> parseFields(String fields){
+        if(fields == null || fields.isEmpty()){
+            return new HashSet<>();
+        }
+        return Arrays.stream(fields.split(", "))
+                .map(Field::fromString)
+                .collect(Collectors.toSet());
+    }
+
+    private Set<Applicant> parseApplicants(String applicants){
+        if(applicants == null || applicants.isEmpty()){
+            return new HashSet<>();
+        }
+        return Arrays.stream(applicants.split(", "))
+                .map(Applicant::fromString)
+                .collect(Collectors.toSet());
+    }
+
+    private Date parseDate(String date){
+        if(date == null || date.isEmpty()){
+            return null;
+        }
+        return Date.valueOf(date);
     }
 }
