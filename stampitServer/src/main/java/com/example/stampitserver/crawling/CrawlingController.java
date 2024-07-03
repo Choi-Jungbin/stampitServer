ab405b9b45c8ba528008ebfff56610fea52c5429
JB <wjdqls6704@naver.com>
2024-07-03 22:46:01 +0900

    feat: 크롤링 컨트롤러 작성

---
+++ stampitServer/src/main/java/com/example/stampitserver/crawling/CrawlingController.java
@@ -0,0 +1,20 @@
+package com.example.stampitserver.crawling;
+
+import com.example.stampitserver.core.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class CrawlingController {
+    private final CrawlingService crawlingService;
+
+    @PostMapping("/crawling")
+    public ResponseEntity<ApiUtils.ApiResult> crawler(String url){
+        crawlingService.crawling(url);
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+}
