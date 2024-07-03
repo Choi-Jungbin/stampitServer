97797be2ecdf8eb95c6f65ca3a6377f68bb59627
JB <wjdqls6704@naver.com>
2024-07-03 23:53:30 +0900

    feat: ContestJPARepository 생성

--- stampitServer/src/main/java/com/example/stampitserver/contest/ContestJPARepository.java
+++ stampitServer/src/main/java/com/example/stampitserver/contest/ContestJPARepository.java
@@ -2,5 +2,8 @@ package com.example.stampitserver.contest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContestJPARepository extends JpaRepository<Contest, Long> {
+
 }
