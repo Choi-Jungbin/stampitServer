aa9f33d734a9574937a6e47b1b595844bc585be4
JB <wjdqls6704@naver.com>
2024-07-08 18:43:36 +0900

    feat: findAll 메서드 작성

--- stampitServer/src/main/java/com/example/stampitserver/contest/ContestJPARepository.java
+++ stampitServer/src/main/java/com/example/stampitserver/contest/ContestJPARepository.java
@@ -1,9 +1,11 @@
 package com.example.stampitserver.contest;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface ContestJPARepository extends JpaRepository<Contest, Long> {
-
+    Page<Contest> findAll(Pageable pageable);
 }
