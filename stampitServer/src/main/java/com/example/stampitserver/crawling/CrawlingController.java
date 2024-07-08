15524fb8e471c70ca6f5b2be17ae15cca106b1bb
JB <wjdqls6704@naver.com>
2024-07-08 18:44:22 +0900

    feat: findAllContest 메서드 작성

--- stampitServer/src/main/java/com/example/stampitserver/crawling/CrawlingController.java
+++ stampitServer/src/main/java/com/example/stampitserver/crawling/CrawlingController.java
@@ -1,10 +1,13 @@
 package com.example.stampitserver.crawling;
 
+import com.example.stampitserver.contest.ContestFindAllResponseDTO;
 import com.example.stampitserver.core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -16,5 +19,12 @@ public class CrawlingController {
     public ResponseEntity<ApiUtils.ApiResult> crawler(String url){
         crawlingService.crawling(url);
         return ResponseEntity.ok(ApiUtils.success("success", null));
+    }
+
+    @GetMapping("/contests")
+    public ResponseEntity<ApiUtils.ApiResult> findAllContest(@PageableDefault(size = 20) Pageable pageable){
+        ContestFindAllResponseDTO contestFindAllResponseDTO = crawlingService.findAllContest(pageable);
+
+        return ResponseEntity.ok(ApiUtils.success(contestFindAllResponseDTO, "success"));
     }
 }
