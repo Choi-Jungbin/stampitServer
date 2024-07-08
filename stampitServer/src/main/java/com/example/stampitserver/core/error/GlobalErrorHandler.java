601a851e5c9090c7590cee7efe2bc487fa18f801
JB <wjdqls6704@naver.com>
2024-07-09 00:06:13 +0900

    feat: GlobalErrorHandler 수정

--- stampitServer/src/main/java/com/example/stampitserver/core/error/GlobalErrorHandler.java
+++ stampitServer/src/main/java/com/example/stampitserver/core/error/GlobalErrorHandler.java
@@ -1,22 +1,21 @@
 package com.example.stampitserver.core.error;
 
 import com.example.stampitserver.core.error.exception.NotFondEnumException;
+import com.example.stampitserver.core.error.exception.NotFoundException;
 import com.example.stampitserver.core.error.exception.OutOfDateException;
+import com.example.stampitserver.core.utils.ApiUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalErrorHandler {
 
-    @ExceptionHandler(OutOfDateException.class)
-    public ResponseEntity<?> outOfDate(OutOfDateException e){
-        return new ResponseEntity<>(e.body(), e.status());
-    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> NotFoundException(NotFoundException e){
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.error("Not Found", e.getMessage(), e.status());
 
-    @ExceptionHandler(NotFondEnumException.class)
-    public ResponseEntity<?> notFoundEnum(NotFondEnumException e){
-        System.out.println(e.getMessage());
-        return new ResponseEntity<>(e.body(), e.status());
+        return new ResponseEntity<>(apiResult, HttpStatus.NOT_FOUND);
     }
 }
